682e3a9650864d9dd1f6907d41df70443201a963
toannguyen5699 <toannguyen050699@gmail.com>
2019-07-24 22:27:45 +0700

    [1.0.0] Add contact realtime

--- public/js/removeRequestContact.js
+++ public/js/removeRequestContact.js
@@ -11,9 +11,21 @@ function removeRequestContact() {
                     $("#find-user").find(`div.user-remove-request-contact[data-uid = ${targetId}]`).hide();
                     $("#find-user").find(`div.user-add-new-contact[data-uid = ${targetId}]`).css("display", "inline-block");
                     decreaseNumberNotifContact("count-request-contact-sent");
-                    // xu ly realtime o bai sau
+                    socket.emit("remove-request-contact", {contactId: targetId});
                 }
             }
         });
     });
 }
+
+
+socket.on("response-remove-request-contact", function(user) {
+    $(".noti_content").find(`span[data-uid = ${user.id}]`).remove();
+
+    // Xoa o modal tab yeu cau ket ban
+
+    decreaseNumberNotifContact("count-request-contact-received");
+
+    decreaseNumberNotification("noti_contact_counter");
+    decreaseNumberNotification("noti_counter");
+})
--- sockets/contact/addNewContact.js
+++ sockets/contact/addNewContact.js
@@ -1,12 +1,33 @@
+import {pushSocketIdToArray, emitNotifyToArray, removeSocketIdFromArray} from "./../../helpers/socketHelper";
+
 /**
  * @param io from socket.io library
  */
 
 let addNewContact = (io) => {
+  let clients = {};
   io.on("connection", (socket) => {
+    // push socket id to array
+    clients = pushSocketIdToArray(clients, socket.request.user._id, socket.id);
+
     socket.on("add-new-contact", (data) => {
-			console.log(data);
-			console.log(socket.request.user);
+      let currentUser = {
+        id: socket.request.user._id,
+        username: socket.request.user.username,
+        avatar: socket.request.user.avatar,
+      };
+
+      // emit notification (chi gui thong bao ve user nhan duoc loi moi ket ban)
+      if (clients[data.contactId]) {
+        // Khi mo 2 tab trinh duyet socketio se gui thong bao ve ca 2
+      emitNotifyToArray(clients, data.contactId, io, "response-add-new-contact", currentUser);
+      }
+      
+    });
+
+    socket.on("disconnect", () => {
+      // Remove socketId when socket disconnect 
+      clients = removeSocketIdFromArray(clients, socket.request.user._id, socket);
     });
   });
 }
