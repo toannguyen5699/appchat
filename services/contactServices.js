6157668f0c31fa94aa14d31ecfeed9a9a00e3133
toannguyen5699 <toannguyen050699@gmail.com>
2019-07-25 23:28:42 +0700

    [1.0.0] Render notifications to view

--- services/contactServices.js
+++ services/contactServices.js
@@ -1,51 +1,65 @@
 import contactModel from "./../models/contactModel";
 import userModel from "./../models/userModel";
+import NotificationModel from "./../models/notificationModel";
 import _ from "lodash";
 
 let findUsersContact = (currentUserId, keyword) => {
-    return new Promise(async (resolve, reject) => {
-        let deprecatedUserIds = [currentUserId]; 
-        let contactsByUser = await contactModel.findAllByUser(currentUserId);
-        contactsByUser.forEach((contact) => {
-            deprecatedUserIds.push(contact.userId);
-            deprecatedUserIds.push(contact.contactId);
-        });
-
-        deprecatedUserIds = _.uniqBy(deprecatedUserIds);
-        let users = await userModel.findAllForAddContact(deprecatedUserIds, keyword);
-        resolve(users);
+  return new Promise(async (resolve, reject) => {
+    let deprecatedUserIds = [currentUserId]; 
+    let contactsByUser = await contactModel.findAllByUser(currentUserId);
+    contactsByUser.forEach((contact) => {
+      deprecatedUserIds.push(contact.userId);
+        deprecatedUserIds.push(contact.contactId);
     });
+
+    deprecatedUserIds = _.uniqBy(deprecatedUserIds);
+    let users = await userModel.findAllForAddContact(deprecatedUserIds, keyword);
+    resolve(users);
+  });
 } 
 
 let addNew = (currentUserId, contactId) => {
-    return new Promise(async (resolve, reject) => {
-        let contactExists = await contactModel.checkExists(currentUserId, contactId);
-        if (contactExists) {
-            return reject(false);
-        }
+  return new Promise(async (resolve, reject) => {
+    let contactExists = await contactModel.checkExists(currentUserId, contactId);
+    if (contactExists) {
+      return reject(false);
+    }
+    // create contact
+    let newContactItem = {
+      userId: currentUserId,
+      contactId: contactId
+    };
 
-        let newContactItem = {
-            userId: currentUserId,
-            contactId: contactId
-        };
+    let newContact = await contactModel.createNew(newContactItem);
 
-        let newContact = await contactModel.createNew(newContactItem);
-        resolve(newContact);
-    });
+    // Create notification
+    let notificationItem = {
+      senderId: currentUserId,
+      receiverId: contactId,
+      type: NotificationModel.types.ADD_CONTACT,
+    };
+		await NotificationModel.model.createNew(notificationItem);
+
+    resolve(newContact);
+  });
 } 
 
 let removeRequestContact = (currentUserId, contactId) => {
-    return new Promise(async (resolve, reject) => {
-        let removeReq = await contactModel.removeRequestContact(currentUserId, contactId);
-        if (removeReq.n === 0) {
-            return reject(false);
-        }
-        resolve(true);
-    });
+  return new Promise(async (resolve, reject) => {
+    let removeReq = await contactModel.removeRequestContact(currentUserId, contactId);
+    if (removeReq.n === 0) {
+      return reject(false);
+		}
+		
+		// remove notification
+		let notifTypeAddContact = NotificationModel.types.ADD_CONTACT;
+		await NotificationModel.model.removeRequestContactNotification(currentUserId, contactId, notifTypeAddContact);
+    resolve(true);
+  });
 } 
 
 module.exports = {
-    findUsersContact: findUsersContact,
-    addNew: addNew,
-    removeRequestContact: removeRequestContact
+  findUsersContact: findUsersContact,
+  addNew: addNew,
+  removeRequestContact: removeRequestContact
 };
--- services/index.js
+++ services/index.js
@@ -1,7 +1,9 @@
 import authService from "./authServices";
 import userService from "./userServices";
-import contactService from "./contactServices"
+import contactService from "./contactServices";
+import notificationService from "./notificationServices";
 
 export const auth = authService;
 export const user = userService;
 export const contact = contactService;
+export const notification = notificationService;
